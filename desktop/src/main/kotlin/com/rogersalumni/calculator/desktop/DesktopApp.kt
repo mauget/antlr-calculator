f262438c168524470137dfd29b602893c8e1e5e2
mauget <mauget@gmail.com>
2019-11-15 21:19:25 -0500

    Stubbed calculator UI buttons, textfield

--- desktop/src/main/kotlin/com/rogersalumni/calculator/desktop/DesktopApp.kt
+++ desktop/src/main/kotlin/com/rogersalumni/calculator/desktop/DesktopApp.kt
@@ -9,19 +9,87 @@ class DesktopView : View() {
     private val controller: DesktopController by inject()
     private val input = SimpleStringProperty()
 
-    override val root = form {
-        fieldset {
-            field("Arithmetic expression") {
-                textfield(input)
-            }
+//    override val root = form {
+//        fieldset {
+//            field("Arithmetic expression") {
+//                textfield(input)
+//            }
+//
+//            button("Evaluate") {
+//                action {
+//                    controller.evalEpression(input.value)
+//                    input.value =""
+//                }
+//            }
+//        }
+//    }
 
-            button("Evaluate") {
-                action {
-                    controller.evalEpression(input.value)
-                    input.value = ""
+    override val root = vbox {
+        hbox {
+            textfield() {
+                textProperty().addListener { _, old, new ->
+                    println("=")
                 }
             }
         }
+        hbox {
+            button("C") {
+            }
+            button("\u00B1") {
+            }
+            button("%") {
+            }
+            button("\u00F7") {
+            }
+        }
+        hbox {
+            button("7") {
+            }
+            button("8") {
+            }
+            button("9") {
+            }
+            button("\u2A09") {
+            }
+        }
+        hbox {
+            button("4") {
+            }
+            button("5") {
+            }
+            button("6") {
+            }
+            button("\u2212") {// +
+            }
+        }
+        hbox {
+            button("1") {
+            }
+            button("2") {
+            }
+            button("3") {
+            }
+            button("\uFF0B") {// +
+            }
+        }
+        hbox {
+            button("0") {
+                style {
+                    minWidth = 4.em
+                }
+            }
+            button(".") {
+            }
+            button("=") {
+            }
+        }
+
+//        button("LOGIN") {
+//            useMaxWidth = true
+//            action {
+//                println("qqqqqq")
+//            }
+//        }
     }
 }
 
@@ -34,11 +102,14 @@ class DesktopController : Controller() {
     }
 }
 
-class DesktopApp : App() {
-    override val primaryView = DesktopView::class
+class DesktopApp : App(DesktopView::class, Styles::class) {
 
     companion object {
         fun create(): DesktopApp = DesktopApp()
+    }
+
+    init {
+        reloadStylesheetsOnFocus()
     }
 }
 
---
+++ desktop/src/main/kotlin/com/rogersalumni/calculator/desktop/Styles.kt
@@ -0,0 +1,35 @@
+package com.rogersalumni.calculator.desktop
+
+import tornadofx.*
+
+class Styles : Stylesheet() {
+    companion object {
+        // Define styles
+        val wrapper by cssclass()
+
+        // Define colors
+//        val dangerColor = c("#a94442")
+//        val hoverColor = c("#d49942")
+    }
+
+    init {
+        wrapper {
+            padding = box(6.px)
+        }
+
+        button {
+            fontSize = 40.px
+            padding = box(5.px, 10.px)
+            minWidth = 2.em
+
+        }
+
+        textField {
+            fontSize = 40.px
+            padding = box(6.px)
+            spacing = 10.px
+            minWidth = 8.em
+            maxWidth = 8.em
+        }
+    }
+}
