13b03341929835b6cdd7d095e492efaf1501d46e
mauget <mauget@gmail.com>
2019-11-17 20:51:38 -0500

    Most buttons wired

--- desktop/src/main/kotlin/com/rogersalumni/calculator/desktop/DesktopApp.kt
+++ desktop/src/main/kotlin/com/rogersalumni/calculator/desktop/DesktopApp.kt
@@ -6,8 +6,11 @@ import javafx.scene.control.TextField
 import tornadofx.*
 
 class DesktopView : View() {
-    private val controller = DesktopController()
     private var calcTextField: TextField by singleAssign()
+    private var expression: String = ""
+
+    private val controller = DesktopController()
+    private val opCodes = setOf("+", "-", "*", "/", "(", ")", "%", "uminus")
 
     override val root = vbox {
         addClass(Styles.wrapper)
@@ -30,12 +33,15 @@ class DesktopView : View() {
                     backgroundColor += Styles.operationBgDarkColor
                 }
                 action {
-                    calcTextField.text = "0"
+                    resetCalculator()
                 }
             }
             button("\u00B1") {
                 style {
                     backgroundColor += Styles.operationBgDarkColor
+                }
+                action {
+                    accept("uminus")
                 }
             }
             button("mod") {
@@ -43,54 +49,94 @@ class DesktopView : View() {
                     fontSize = 28.px
                     backgroundColor += Styles.operationBgDarkColor
                 }
+                action {
+                    accept("%")
+                }
             }
             button("\u00F7") {
                 style {
                     backgroundColor += Styles.operationBgColor
+                }
+                action {
+                    accept("/")
                 }
             }
         }
         hbox {
             button("7") {
                 action {
-                    println("7 pressed")
+                    accept("7")
                 }
             }
             button("8") {
+
+                action {
+                    accept("8")
+                }
             }
             button("9") {
+                action {
+                    accept("9")
+                }
             }
             button("\u2A09") {
                 style {
                     backgroundColor += Styles.operationBgColor
                 }
+                action {
+                    accept("+")
+                }
             }
         }
         hbox {
             button("4") {
+                action {
+                    accept("4")
+                }
             }
             button("5") {
+                action {
+                    accept("5")
+                }
             }
             button("6") {
+                action {
+                    accept("6")
+                }
             }
             button("\u2212") {
                 // -
                 style {
                     backgroundColor += Styles.operationBgColor
                 }
+                action {
+                    accept("-")
+                }
             }
         }
         hbox {
             button("1") {
+                action {
+                    accept("1")
+                }
             }
             button("2") {
+                action {
+                    accept("2")
+                }
             }
             button("3") {
+                action {
+                    accept("3")
+                }
             }
             button("\uFF0B") {
                 // +
                 style {
                     backgroundColor += Styles.operationBgColor
+                }
+                action {
+                    accept("+")
                 }
             }
         }
@@ -99,27 +145,56 @@ class DesktopView : View() {
                 style {
                     minWidth = 168.px
                 }
+                action {
+                    accept("0")
+                }
             }
             button(".") {
+                action {
+                    accept(".")
+                }
             }
             button("=") {
                 style {
                     backgroundColor += Styles.operationBgColor
                 }
                 action {
-                    val mockInput = "-1+2*20+1"
-                    println(mockInput + " = " + controller.evalEpression(mockInput))
+                    calcTextField.text = controller.evalEpression(expression).toString()
+                    expression = calcTextField.text
                 }
             }
         }
 
+    }
+
+    init {
+        this.title = "Calculator"
+        this.resetCalculator()
+    }
+
+    private fun resetCalculator() {
+        expression = ""
+        calcTextField.text = "0"
+    }
+
+    private fun accept(arg: String) {
+        expression += arg
+
+        if (calcTextField.text == "0") {
+            calcTextField.text = ""
+        }
+        if (!opCodes.contains(arg)) {
+            calcTextField.text += arg
+        } else {
+            calcTextField.text = ""
+        }
     }
 }
 
 class DesktopController : Controller() {
     private val calcEngine: CalculatorApp = CalculatorAppImpl()
 
-    fun evalEpression(inputValue: String) : Double {
+    fun evalEpression(inputValue: String): Double {
         return calcEngine.calculate(inputValue)
     }
 }
