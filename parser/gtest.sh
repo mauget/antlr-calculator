c2a7b4a4c30f9907f32a561b31fd32bbcdc3180e
Louis E Mauget <lmauget@keyholesoftware.com>
2019-11-30 09:28:55 -0500

    Make Windows bash stop complaining about bare \r

--- parser/gtest.sh
+++ parser/gtest.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+#
 #-- Antlr4 path (here, to Mac Homebrew tree)
 CP=/usr/local/Cellar/antlr/4.7.2/antlr-4.7.2-complete.jar
-
+#
 #-- Append Grammar path (here, to generated classes in Maven target)
 CP="${CP}:/Users/mauget/IdeaProjects/calculator/parser/target/classes"
-
+#
 #-- Call TestRig with command line parameters
 #-- E.g. ./gtest.sh com.rogersalumni.calculator.g4.Calculator start -gui <expression.txt
 java -cp ${CP} org.antlr.v4.gui.TestRig "$@"
