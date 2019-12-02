e7537fd23559a43dab088ecf80a58a9fa97bd378
mauget <mauget@gmail.com>
2019-12-01 20:48:28 -0500

    Comment where to run the script

--- parser/gtest.sh
+++ parser/gtest.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
+# Run this script from parser project root
 #
-#-- Antlr4 path (here, to Mac Homebrew tree)
+#-- Antlr4 path (here, installed by Mac Homebrew)
 CP=/usr/local/Cellar/antlr/4.7.2/antlr-4.7.2-complete.jar
 #
-#-- Append Grammar path (here, to generated classes in Maven target)
-CP="${CP}:/Users/mauget/IdeaProjects/antlr-calculator/parser/target/classes"
+#-- Append Grammar path relative to project's Maven target classes
+CP="${CP}:./target/classes"
 #
 #-- Call TestRig with command line parameters
 #-- E.g. ./gtest.sh com.rogersalumni.calculator.g4.Calculator start -gui <expression.txt
